792d1c5545051ffc6f7da1bf58acfbcae822ee27
splitPoleDAQ <splitPoleDAQ@notexist.com>
2024-01-19 03:07:16 -0500

    Added traceLength in Hit Class

--- Hit.h
+++ Hit.h
@@ -3,14 +3,15 @@
 
 class Hit{
 public:
-  int sn;
-  unsigned short bd;
-  unsigned short ch;
+  unsigned short sn;
+  uint8_t bd;
+  uint8_t ch;
   unsigned short energy;
   unsigned short energy2;
   unsigned long long timestamp;
   unsigned short fineTime;
 
+  unsigned short traceLength;
   std::vector<short> trace;
 
   Hit(){
@@ -25,11 +26,18 @@ public:
     energy2 = 0;
     timestamp = 0;
     fineTime = 0;
+    traceLength = 0;
     trace.clear();
   }
 
   void Print(){
     printf("(%5d, %2d) %6d %16llu, %6d, %5ld\n", sn, ch, energy, timestamp, fineTime, trace.size());
+  }
+
+  void PrintTrace(){
+    for( unsigned short i = 0; i < traceLength; i++){
+      printf("%3u | %6d \n", i, trace[i]);
+    }
   }
 
    // Define operator< for sorting
